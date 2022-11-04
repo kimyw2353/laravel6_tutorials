7ac2503f4a777dc3166d15437e10b05eada7452e
kimyw2353 <kimyw2353@naver.com>
2022-11-04 14:58:05 +0900

    foreach 사용해서 리스트 출력

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -69,25 +69,42 @@
                 <div class="title m-b-md">
                     Pizzas
                 </div>
-                <div style="display: none">
-                    <p>{{$type}} - {{$base}} - {{$price}}$</p>
-                    @if($price > 15)
-                        <p>this pizza is expensive</p>
-                    @elseif($price < 5)
-                        <p>this pizza is cheap</p>
-                    @else
-                        <p>this pizza is normally priced</p>
-                    @endif
+{{--                 <div style="display: none"> --}}
+{{--                     <p>{{$type}} - {{$base}} - {{$price}}$</p> --}}
+{{--                     @if($price > 15) --}}
+{{--                         <p>this pizza is expensive</p> --}}
+{{--                     @elseif($price < 5) --}}
+{{--                         <p>this pizza is cheap</p> --}}
+{{--                     @else --}}
+{{--                         <p>this pizza is normally priced</p> --}}
+{{--                     @endif --}}
 
-                    @unless($base == 'cheesy crust')
-                        <p>you don't have a cheesy crust</p>
-                    @endunless
+{{--                     @unless($base == 'cheesy crust') --}}
+{{--                         <p>you don't have a cheesy crust</p> --}}
+{{--                     @endunless --}}
 
-                    @php
-                        $name = 'shoun';
-                        echo($name);
-                    @endphp
-                </div>
+{{--                     @php --}}
+{{--                         $name = 'shoun'; --}}
+{{--                         echo($name); --}}
+{{--                     @endphp --}}
+{{--                 </div> --}}
+{{--                @for($i = 0; $i < 5; $i++)--}}
+{{--                    <p>the value of i is {{ $i }}</p>--}}
+{{--                @endfor--}}
+{{--                @for($i = 0; $i < count($pizzas); $i++)--}}
+{{--                    <p>{{ $pizzas[$i]['type'] }}</p>--}}
+{{--                @endfor--}}
+                @foreach($pizzas as $pizza)
+                    <div>
+                        {{ $loop->index }} {{ $pizza['type'] }} - {{ $pizza['base'] }}
+                        @if($loop->first)
+                            <span>- first in the loop</span>
+                        @endif
+                        @if($loop->last)
+                            <span>- last in the loop</span>
+                        @endif
+                    </div>
+                @endforeach
             </div>
         </div>
     </body>

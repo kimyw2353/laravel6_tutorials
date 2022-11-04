8509c91babcea9a26b24a7453f51964c0e313880
kimyw2353 <kimyw2353@naver.com>
2022-11-04 16:12:50 +0900

    layout 파일 생성

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <!-- Styles -->
+        <link rel="stylesheet" href="/css/main.css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        @yield('content')
+        <footer>
+            Copyright 2020 Pizza House.
+        </footer>
+    </body>
+</html>

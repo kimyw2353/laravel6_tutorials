646eedea3a350092fd8a92b40b2b1e7a200dc5e6
kimyw2353 <kimyw2353@naver.com>
2022-11-04 14:11:59 +0900

    if, unless, php 문법 예시

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -65,24 +65,27 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     Pizzas
                 </div>
+                <p>{{$type}} - {{$base}} - {{$price}}$</p>
+                @if($price > 15)
+                    <p>this pizza is expensive</p>
+                @elseif($price < 5)
+                    <p>this pizza is cheap</p>
+                @else
+                    <p>this pizza is normally priced</p>
+                @endif
+
+                @unless($base == 'cheesy crust')
+                    <p>you don't have a cheesy crust</p>
+                @endunless
+
+                @php
+                    $name = 'shoun';
+					echo($name);
+                @endphp
             </div>
         </div>
     </body>

52966a3dd2bd6197510632da07818275515c3235
kimyw2353 <kimyw2353@naver.com>
2022-11-04 14:43:26 +0900

    예시부분 display none설정

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -69,23 +69,25 @@
                 <div class="title m-b-md">
                     Pizzas
                 </div>
-                <p>{{$type}} - {{$base}} - {{$price}}$</p>
-                @if($price > 15)
-                    <p>this pizza is expensive</p>
-                @elseif($price < 5)
-                    <p>this pizza is cheap</p>
-                @else
-                    <p>this pizza is normally priced</p>
-                @endif
+                <div style="display: none">
+                    <p>{{$type}} - {{$base}} - {{$price}}$</p>
+                    @if($price > 15)
+                        <p>this pizza is expensive</p>
+                    @elseif($price < 5)
+                        <p>this pizza is cheap</p>
+                    @else
+                        <p>this pizza is normally priced</p>
+                    @endif
 
-                @unless($base == 'cheesy crust')
-                    <p>you don't have a cheesy crust</p>
-                @endunless
+                    @unless($base == 'cheesy crust')
+                        <p>you don't have a cheesy crust</p>
+                    @endunless
 
-                @php
-                    $name = 'shoun';
-					echo($name);
-                @endphp
+                    @php
+                        $name = 'shoun';
+                        echo($name);
+                    @endphp
+                </div>
             </div>
         </div>
     </body>
